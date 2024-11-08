3b7915bb8d34b6539ae42482178df16712b28e37
glafiro <dglafiro@gmail.com>
2024-11-08 18:33:06 +0100

    updated build instructions

--- JuceLibraryCode/JucePluginDefines.h
+++ JuceLibraryCode/JucePluginDefines.h
@@ -155,7 +155,7 @@
  #define JucePlugin_ARAFactoryID           "com.RedgooseAudio.Vibee.factory"
 #endif
 #ifndef  JucePlugin_ARADocumentArchiveID
- #define JucePlugin_ARADocumentArchiveID   "com.RedgooseAudio.Vibee.aradocumentarchive.0.0.1"
+ #define JucePlugin_ARADocumentArchiveID   "com.RedgooseAudio.Vibee.aradocumentarchive.0.1.0"
 #endif
 #ifndef  JucePlugin_ARACompatibleArchiveIDs
  #define JucePlugin_ARACompatibleArchiveIDs  ""
